2fa0c2de352a7bc816705bf6c7fdf1f318b3141f
Moises Renteria <moy@Moisess-Air.attlocal.net>
2022-05-25 23:14:47 -0500

    day2 register editprofile and login form created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Button from './components/Button';
 import NavBar from './components/NavBar';
 import BookCard from './components/Card';
+import RegisterForm from './forms/RegisterForm';
+import LoginForm from './forms/LoginForm';
 
 function App() {
   return (
     <NavBar>
         <Button color="primary">Button</Button>
         <BookCard/>
+
+        <RegisterForm/>
     </NavBar>
   );
 }
